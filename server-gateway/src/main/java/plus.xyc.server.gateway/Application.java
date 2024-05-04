ef375a2bc1adc8a2a75633613c544da59f99ecf4
Grant <284885069@qq.com>
2024-05-04 22:56:49 +0800

    feat: add gateway

--- server-gateway/src/main/java/plus.xyc.server.gateway/Application.java
+++ server-gateway/src/main/java/plus.xyc.server.gateway/Application.java
@@ -1,15 +1,11 @@
-package plus.xyc.server.main;
+package plus.xyc.server.gateway;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"plus.xyc.server", "org.zkit.support"})
-@MapperScan("plus.xyc.server.main.*.mapper")
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = {"org.zkit.support"})
 public class Application {
 
     public static void main(String[] args) {
