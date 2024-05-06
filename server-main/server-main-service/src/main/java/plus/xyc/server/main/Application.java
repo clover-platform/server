b8229ce6515c04bb8c723cadc715a283f907158b
Grant <284885069@qq.com>
2024-05-06 21:03:58 +0800

    feat: 方法自调用

--- server-main/server-main-service/src/main/java/plus/xyc/server/main/Application.java
+++ server-main/server-main-service/src/main/java/plus/xyc/server/main/Application.java
@@ -3,15 +3,18 @@ package plus.xyc.server.main;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(scanBasePackages = {"org.zkit.support", "plus.xyc.server"})
 @MapperScan("plus.xyc.server.main.*.mapper")
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"org.zkit.support"})
-@EnableAspectJAutoProxy
+@EnableCaching(mode = AdviceMode.ASPECTJ)
+@EnableAspectJAutoProxy(exposeProxy=true)
 public class Application {
 
     public static void main(String[] args) {
