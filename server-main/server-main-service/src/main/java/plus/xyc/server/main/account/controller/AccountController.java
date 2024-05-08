d68721418dc9dbe22828a256c8d4cf4754a233e2
Grant <284885069@qq.com>
2024-05-09 01:02:08 +0800

    feat: otp info

--- server-main/server-main-service/src/main/java/plus/xyc/server/main/account/controller/AccountController.java
+++ server-main/server-main-service/src/main/java/plus/xyc/server/main/account/controller/AccountController.java
@@ -4,9 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.zkit.support.cloud.starter.auth.annotation.PublicRequest;
+import org.zkit.support.cloud.starter.entity.Result;
 import org.zkit.support.server.account.api.entity.response.AccountResponse;
+import org.zkit.support.server.account.api.entity.response.OTPResponse;
 import org.zkit.support.server.account.api.entity.response.TokenResponse;
 import org.zkit.support.server.account.api.rest.AuthAccountApi;
+import org.zkit.support.throttler.starter.annotation.Throttler;
 import plus.xyc.server.main.account.entity.request.CheckRegisterEmailRequest;
 import plus.xyc.server.main.account.entity.request.SendRegisterEmailRequest;
 import plus.xyc.server.main.account.service.AccountService;
@@ -30,9 +33,9 @@ public class AccountController {
     @PublicRequest
     @GetMapping("/test")
     public AccountResponse test(@RequestParam("username") String username) {
-        AccountResponse response = authAccountApi.findByUsername(username);
+        Result<AccountResponse> response = authAccountApi.findByUsername(username);
         log.info("AccountController test {}", response);
-        return response;
+        return response.getData();
     }
 
     @PublicRequest
@@ -43,8 +46,16 @@ public class AccountController {
 
     @PublicRequest
     @PostMapping("/register/email/check")
+    @Throttler(value = "register", limit = 5)
     public TokenResponse checkRegisterEmail(@RequestBody CheckRegisterEmailRequest request) {
         return this.accountService.checkRegisterEmail(request);
+    }
+
+    @PublicRequest
+    @GetMapping("/otp/secret")
+    public OTPResponse otpSecret() {
+        Result<OTPResponse> response = authAccountApi.otpSecret(1L);
+        return response.getData();
     }
 
     @Autowired
