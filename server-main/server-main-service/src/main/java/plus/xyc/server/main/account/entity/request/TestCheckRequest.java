4546a7b2dd0704a40be8688ac213900389d25639
Grant <284885069@qq.com>
2024-05-11 10:50:49 +0800

    feat: open api

--- server-main/server-main-service/src/main/java/plus/xyc/server/main/account/entity/request/TestCheckRequest.java
+++ server-main/server-main-service/src/main/java/plus/xyc/server/main/account/entity/request/TestCheckRequest.java
@@ -1,18 +1,23 @@
 package plus.xyc.server.main.account.entity.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@Schema(description = "测试表单验证")
 public class TestCheckRequest {
 
     @NotNull
     @Min(100)
+    @Schema(description = "大小")
     private Integer size;
+
     @Email
     @NotNull
+    @Schema(description = "邮箱")
     private String email;
 
 }
