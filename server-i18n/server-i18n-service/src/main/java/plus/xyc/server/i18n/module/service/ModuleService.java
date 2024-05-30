01db959417681ccd6ab6d0fbce5d08c38bd1bf1f
Grant <284885069@qq.com>
2024-05-30 19:49:47 +0800

    feat: 查询细节

--- server-i18n/server-i18n-service/src/main/java/plus/xyc/server/i18n/module/service/ModuleService.java
+++ server-i18n/server-i18n-service/src/main/java/plus/xyc/server/i18n/module/service/ModuleService.java
@@ -2,10 +2,10 @@ package plus.xyc.server.i18n.module.service;
 
 import org.zkit.support.starter.mybatis.entity.PageQueryRequest;
 import org.zkit.support.starter.mybatis.entity.PageResult;
-import plus.xyc.server.i18n.language.entity.response.LanguageResponse;
 import plus.xyc.server.i18n.module.entity.dto.Module;
 import com.baomidou.mybatisplus.extension.service.IService;
-import plus.xyc.server.i18n.module.entity.request.CreateModuleRequest;
+import plus.xyc.server.i18n.module.entity.request.ModuleAllRequest;
+import plus.xyc.server.i18n.module.entity.request.ModuleCreateRequest;
 import plus.xyc.server.i18n.module.entity.request.ModuleQueryRequest;
 import plus.xyc.server.i18n.module.entity.response.ModuleDashboardResponse;
 import plus.xyc.server.i18n.module.entity.response.ModuleLanguageResponse;
@@ -24,8 +24,9 @@ import java.util.List;
 public interface ModuleService extends IService<Module> {
 
     PageResult<ModuleResponse> query(PageQueryRequest page, ModuleQueryRequest query);
-    void create(CreateModuleRequest request);
+    void create(ModuleCreateRequest request);
     ModuleDashboardResponse dashboard(Long id);
     List<ModuleLanguageResponse>  languages(Long id);
+    List<ModuleResponse> all(ModuleAllRequest request);
 
 }
