1df6bdfa50e0ad328bef204d5ae08a48fe0fd6d9
grant <284885069@qq.com>
2024-06-07 16:10:01 +0800

    feat: 用户信息支持模糊搜索

---
+++ server-main/server-main-api/src/main/java/plus/xyc/server/main/api/entity/request/ApiAccountListRequest.java
@@ -0,0 +1,21 @@
+package plus.xyc.server.main.api.entity.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Schema(description = "用户列表请求")
+public class ApiAccountListRequest {
+
+    @Schema(description = "用户ID列表")
+    private List<Long> ids;
+    @Schema(description = "关键字")
+    private String keyword;
+    @Schema(description = "每页大小")
+    private Integer size = 100;
+    @Schema(description = "页码")
+    private Integer page = 1;
+
+}
--- server-main/server-main-api/src/main/java/plus/xyc/server/main/api/rest/MainAccountRestApi.java
+++ server-main/server-main-api/src/main/java/plus/xyc/server/main/api/rest/MainAccountRestApi.java
@@ -1,13 +1,17 @@
 package plus.xyc.server.main.api.rest;
 
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zkit.support.starter.boot.entity.Result;
 import org.zkit.support.starter.mybatis.entity.PageResult;
 import plus.xyc.server.main.api.constant.MainApi;
 import plus.xyc.server.main.api.constant.MainApiRoute;
+import plus.xyc.server.main.api.entity.request.ApiAccountListRequest;
 import plus.xyc.server.main.api.entity.response.ApiAccountResponse;
 
 import java.util.List;
@@ -18,11 +22,8 @@ public interface MainAccountRestApi {
 
     @GetMapping(value = MainApiRoute.ACCOUNT_GET_BY_ID)
     Result<ApiAccountResponse> getById(@RequestParam("id") Long id);
+
     @GetMapping(value = MainApiRoute.ACCOUNT_LIST)
-    Result<PageResult<ApiAccountResponse>> list(
-            @RequestParam("ids") List<Long> ids,
-            @RequestParam(value = "size", defaultValue = "100") Integer size,
-            @RequestParam(value = "page", defaultValue = "1") Integer page
-    );
+    Result<PageResult<ApiAccountResponse>> list(@SpringQueryMap @ParameterObject @ModelAttribute ApiAccountListRequest request);
 
 }
