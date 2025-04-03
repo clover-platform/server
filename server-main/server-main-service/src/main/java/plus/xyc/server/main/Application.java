0c27a2e8eb9e8a647c155555af0187c631616417
Grant <284885069@qq.com>
2025-04-03 18:43:19 +0800

    feat: tracing

--- server-main/server-main-service/src/main/java/plus/xyc/server/main/Application.java
+++ server-main/server-main-service/src/main/java/plus/xyc/server/main/Application.java
@@ -3,6 +3,7 @@ package plus.xyc.server.main;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.observation.web.servlet.WebMvcObservationAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
