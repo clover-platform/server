465db4f147f1008353804f660a690330de047852
Grant <284885069@qq.com>
2025-04-22 14:04:14 +0800

    feat: warning

--- server-wiki/server-wiki-service/src/main/java/plus/xyc/server/wiki/book/entity/mapstruct/BookMapStruct.java
+++ server-wiki/server-wiki-service/src/main/java/plus/xyc/server/wiki/book/entity/mapstruct/BookMapStruct.java
@@ -2,11 +2,12 @@ package plus.xyc.server.wiki.book.entity.mapstruct;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 import plus.xyc.server.wiki.book.entity.dto.Book;
 import plus.xyc.server.wiki.book.entity.request.CreateBookRequest;
 import plus.xyc.server.wiki.book.entity.response.BookResponse;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BookMapStruct {
 
     Book toBook(CreateBookRequest request);
--- server-wiki/server-wiki-service/src/main/java/plus/xyc/server/wiki/page/entity/mapstruct/PageStruct.java
+++ server-wiki/server-wiki-service/src/main/java/plus/xyc/server/wiki/page/entity/mapstruct/PageStruct.java
@@ -2,11 +2,12 @@ package plus.xyc.server.wiki.page.entity.mapstruct;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 import plus.xyc.server.wiki.page.entity.dto.Page;
 import plus.xyc.server.wiki.page.entity.response.CatalogResponse;
 import plus.xyc.server.wiki.page.entity.response.PageDetailResponse;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PageStruct {
 
     CatalogResponse toCatalogResponse(Page page);
