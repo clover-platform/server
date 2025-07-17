e3c0859adfff5e629c13ba6b21b7d4d78a4212e4
Grant <284885069@qq.com>
2025-07-18 01:00:23 +0800

    feat: Mapping ignore

--- server-i18n/server-i18n-service/src/main/java/plus/xyc/server/i18n/activity/entity/mapstruct/ActivityMapStruct.java
+++ server-i18n/server-i18n-service/src/main/java/plus/xyc/server/i18n/activity/entity/mapstruct/ActivityMapStruct.java
@@ -1,6 +1,7 @@
 package plus.xyc.server.i18n.activity.entity.mapstruct;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 import plus.xyc.server.i18n.activity.entity.request.ActivityListRequest;
@@ -9,7 +10,10 @@ import plus.xyc.server.i18n.activity.entity.response.ActivityResponse;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ActivityMapStruct {
 
+    @Mapping(target = "metadata", ignore = true)
     org.zkit.support.server.message.api.entity.request.ActivityListRequest toActivityListRequest(ActivityListRequest activity);
+
+    @Mapping(target = "account", ignore = true)
     ActivityResponse toActivityResponse(org.zkit.support.server.message.api.entity.response.ActivityResponse activity);
 
 }
